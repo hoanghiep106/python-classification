f0d09b9d1412203044546e7253234404d6d0407b
Hoang Hiep Nguyen <hoanghiepnguyen106@gmail.com>
2018-03-27 00:51:00 +0700

    Add categories

--- csv_loader.py
+++ csv_loader.py
@@ -1,5 +1,48 @@
 import csv
 
+categories = {
+	'Transportation': {
+		'value': 0,
+		'subcategory': {
+			'Means of transport': 0,
+			'Transportation Infrastructure': 1,
+			'Transportation Services': 2,
+		}
+	},
+	'Accommodation': {
+		'value': 1,
+		'subcategory': {
+			'First class accommodation': 3,
+			'Second class accommodation': 4,
+		}
+	},
+	'Attraction': {
+		'value': 2,
+		'subcategory': {
+			'Culture & Tradition': 5,
+			'Scenic': 6,
+			'Exhibition entertainment': 7,
+			'Live entertainment': 8,
+			'Shopping': 9,
+			'Adventure tours and activities': 10,
+		}
+	},
+	'Service and Ancillary': {
+		'value': 3,
+		'subcategory': {
+			'Information & Guiding': 11,
+			'Catering Service': 12,
+			'Facility': 13,
+		}
+	},
+	'Tourism broker': {
+		'value': 4,
+		'subcategory': {
+			'Tourism agent & operator': 14,
+		}
+	}
+}
+
 tweets = []
 
 with open('tweets-category.csv', encoding="iso-8859-1") as tweet_file:
@@ -7,6 +50,20 @@ with open('tweets-category.csv', encoding="iso-8859-1") as tweet_file:
 	for row in reader:
 		tweet = {
 			'text': row['Tweet'],
-			'subcategory': row['Subcategory'],
+			'subcategory': categories[row['Category']]['subcategory'][row['Subcategory']],
+			'category': categories[row['Category']]['value']
 		}
 		tweets.append(tweet)
+
+
+test_tweets = []
+
+with open('test-tweets-category.csv', encoding="iso-8859-1") as test_tweet_file:
+	reader = csv.DictReader(test_tweet_file)
+	for row in reader:
+		tweet = {
+			'text': row['Tweet'],
+			'subcategory': categories[row['Category']]['subcategory'][row['Subcategory']],
+			'category': categories[row['Category']]['value']
+		}
+		test_tweets.append(tweet)
--- training.py
+++ training.py
@@ -5,8 +5,8 @@ import torchvision.transforms as transforms
 from torch.autograd import Variable
 
 # Hyper Parameters
-input_size = 500
-num_classes = 5
+input_size = 784
+num_classes = 10
 num_epochs = 5
 batch_size = 100
 learning_rate = 0.001
@@ -16,7 +16,7 @@ train_dataset = dsets.MNIST(root='./data', train=True, transform=transforms.ToTe
 
 test_dataset = dsets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
 
-# Dataset Loader (Input Pipeline)
+# Dataset Loader (Input Pipline)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
@@ -38,7 +38,7 @@ model = LogisticRegression(input_size, num_classes)
 # Loss and Optimizer
 # Softmax is internally computed.
 # Set parameters to be updated.
-criterion = nn.CrossEntropyLoss()
+criterion = nn.L1Loss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # Training the Model
@@ -46,7 +46,6 @@ for epoch in range(num_epochs):
 	for i, (images, labels) in enumerate(train_loader):
 		images = Variable(images.view(-1, 28 * 28))
 		labels = Variable(labels)
-		
 		# Forward + Backward + Optimize
 		optimizer.zero_grad()
 		outputs = model(images)
@@ -55,7 +54,8 @@ for epoch in range(num_epochs):
 		optimizer.step()
 		
 		if (i + 1) % 100 == 0:
-			print('Epoch: [%d/%d], Step: [%d/%d], Loss: %.4f' % (epoch + 1, num_epochs, i + 1, len(train_dataset) // batch_size, loss.data[0]))
+			print('Epoch: [%d/%d], Step: [%d/%d], Loss: %.4f' % (
+			epoch + 1, num_epochs, i + 1, len(train_dataset) // batch_size, loss.data[0]))
 
 # Test the Model
 correct = 0
@@ -67,7 +67,7 @@ for images, labels in test_loader:
 	total += labels.size(0)
 	correct += (predicted == labels).sum()
 
-print('Accuracy of the model on the 100 test tweets: %d %%' % (100 * correct / total))
+print('Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
 
 # Save the Model
 torch.save(model.state_dict(), 'model.pkl')
